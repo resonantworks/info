b4d7e156760f8cbc838608eaf1604fa17fc4753e
Matthew Brown <14838752+resonantworks@users.noreply.github.com>
2026-05-05 15:58:05 +0100

    fedora - updated cmake toolchain files

--- systems/assets/toolchain-linux-gcc.cmake
+++ systems/assets/toolchain-linux-gcc.cmake
@@ -2,5 +2,11 @@ set(CMAKE_C_COMPILER "gcc")
 set(CMAKE_CXX_COMPILER "g++")
 set(CMAKE_LINKER "ld")
 
-set(CMAKE_C_FLAGS "-m32 -isystem /usr/include/c++/15/i686-redhat-linux")
-set(CMAKE_CXX_FLAGS "-m32 -isystem /usr/include/c++/15/i686-redhat-linux")
+set(CMAKE_C_FLAGS_INIT "-m32")
+set(CMAKE_CXX_FLAGS_INIT "-m32")
+
+# Workaround for Fedora missing 32-bit headers issue
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux" AND EXISTS "/usr/include/c++/15/i686-redhat-linux")
+  string(APPEND CMAKE_C_FLAGS_INIT " -isystem /usr/include/c++/15/i686-redhat-linux")
+  string(APPEND CMAKE_CXX_FLAGS_INIT " -isystem /usr/include/c++/15/i686-redhat-linux")
+endif()
